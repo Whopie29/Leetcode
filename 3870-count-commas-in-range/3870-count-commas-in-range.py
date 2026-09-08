2a83225f2e4ebbeeceee7dbb59f5f6948e896ab0
GAURAV MALIK <130136530+Whopie29@users.noreply.github.com>
2026-09-08 23:34:47 +0530

    Time: 0 ms (100%), Space: 12.5 MB (32.04%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.py
+++ 3870-count-commas-in-range/3870-count-commas-in-range.py
@@ -4,10 +4,5 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        res = 0
-        for a in range(1, n + 1):
-            if a > 999:
-                res += 1
-        return res
-
+        return max(n-999,0)
         
